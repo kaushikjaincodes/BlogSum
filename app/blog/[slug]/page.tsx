38451ed8c9186e7a3021624489879d4afbd2917b
Shivanjali <shivanjalisriv@gmail.com>
2024-12-26 13:38:08 +0530

    og

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,38 +1,37 @@
-import React, { Suspense } from 'react';
-import Image from 'next/image';
-import styles from './singlePost.module.css';
+import React from 'react'
+import Image from 'next/image'
+import styles from "./singlePost.module.css"
+import { Suspense } from 'react';
 import PostUser from '@/components/postUser/postUser';
 import { getPost } from '@/app/lib/data';
 
-export default async function SinglePostPage({
-  params,
-}: {
-  params: { slug: string }
-}) {
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+// const getPost = async(slug: string) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+//   if(!res.ok) {
+//     throw new Error("Something went wrong")
+//   }
+//   return res.json()
+// }
+
+const SinglePostPage = async ({params}: Params) => {
   const { slug } = params;
   const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/28297970/pexels-photo-28297970/free-photo-of-a-totoro-statue-is-sitting-on-a-bench-outside-a-restaurant.jpeg"
-          alt=""
-          width={250}
-          height={550}
-          className={styles.img}
-        />
+        <Image src="https://images.pexels.com/photos/28297970/pexels-photo-28297970/free-photo-of-a-totoro-statue-is-sitting-on-a-bench-outside-a-restaurant.jpeg" alt='' width={250} height={550} className={styles.img} />
       </div>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            className={styles.avatar}
-            src="https://images.pexels.com/photos/28297970/pexels-photo-28297970/free-photo-of-a-totoro-statue-is-sitting-on-a-bench-outside-a-restaurant.jpeg"
-            alt=""
-            width={50}
-            height={50}
-          />
+          <Image className={styles.avatar} src="https://images.pexels.com/photos/28297970/pexels-photo-28297970/free-photo-of-a-totoro-statue-is-sitting-on-a-bench-outside-a-restaurant.jpeg" alt='' width={50} height={50} />
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userID={post.userId} />
@@ -46,5 +45,7 @@ export default async function SinglePostPage({
         <div className={styles.content}>{post.body}</div>
       </div>
     </div>
-  );
+  )
 }
+
+export default SinglePostPage
